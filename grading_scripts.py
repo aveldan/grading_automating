1375391b45c8676f69fb54f2d058c73bbda7ae73
aveldan <aveldan@iu.edu>
2023-09-26 16:56:20 -0400

    Scripts to fetch the python files added

--- grading_scripts.py
+++ grading_scripts.py
@@ -1,4 +1,87 @@
+import requests
 import os
+import filecmp
+
+authorization_val = 'Secret auth key'
+
+def get_my_students(ass_num):
+    
+    students_info_url = "The url"
+    req_url = students_info_url + str(ass_num)
+    head_dict = {}
+    head_dict['Authorization'] = authorization_val
+    response = requests.get(url=req_url,headers=head_dict)
+
+    if response.status_code != 200:
+        print("Not a 200 response")
+        print(response)
+        return None
+    
+    return response.json()["data"]
+
+def get_the_latest_submission(std_id, ass_num):
+    submissions_list_url = "The url"+str(ass_num)+"conti url"+str(std_id)
+    head_dict = {}
+    head_dict['Authorization'] = authorization_val
+    response = requests.get(url=submissions_list_url, headers=head_dict)
+
+    if response.status_code != 200:
+        print("Not a 200 response")
+        print(response)
+        return None
+    
+    submissions = response.json()["data"]
+    submissions = sorted(submissions, key=lambda x: x['created_at'])
+    latest_submission = submissions[len(submissions)-1]
+
+    return latest_submission
+
+def download_py(std_id, path, filename, ass_num):
+    
+    latest_submission = get_the_latest_submission(std_id, ass_num)
+    
+    file_download_url = "The url"+latest_submission["FilePath"]
+    head_dict = {}
+    head_dict['Authorization'] = authorization_val
+    response = requests.get(url=file_download_url, headers=head_dict)
+    
+    if response.status_code != 200:
+        print("Not a 200 response")
+        print(response)
+        return None
+
+    flptr = open(path+filename+'/'+filename+".py", "w")
+    flptr.write(response.text)
+
+    flptr.close()
+
+    return latest_submission
+
+def get_py_files(ass_num, path):
+    std_list = get_my_students(ass_num=ass_num)
+    
+    if std_list == None:
+        return
+    
+    std_dict = {}
+    for student in std_list:
+        tmp_string = ""
+        tmp_list = student["Fullname"].lower().split(' ')
+        tmp_string += tmp_list[len(tmp_list)-1]
+        for i in range(0,len(tmp_list)-1):
+            tmp_string += tmp_list[i]
+        std_dict[student["StudentId"]] = tmp_string
+
+    last_subs = {}
+    for key, elem in std_dict.items():
+        last = download_py(key,path,elem,ass_num)
+        if last == None:
+            return None
+        
+        last_subs[elem] = last
+
+    return last_subs    
+
 
 def change_filenames(path):
     name_dict = {}
@@ -41,6 +124,17 @@ def create_file_struct(path):
             dst = path+key+'/'+fl
             os.rename(src,dst)
 
+def compare(path, std_dict):
+    
+    same_or_not = {}
+
+    for key in std_dict.keys():
+        file1 = path+key+'/'+key+".py"
+        file2 = path+key+'/'+key+".txt"
+        same_or_not[key] = filecmp.cmp(file1, file2)
+    
+    # Now use this filecmp to create a csv file for same files or not
+    return same_or_not
 
 if __name__ == "__main__":
     #For testing
